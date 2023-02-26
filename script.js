154fe239b3cbadc566c3f454ee13a044290debaf
goncu <116492383+goncu@users.noreply.github.com>
2023-02-26 18:19:18 +0200

    Add ending game

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //variables
 let numberOfRows = 0,
   numberOfColumns = 0,
-  numberOfMines = 0;
+  numberOfMines = 0,
+  gameStatus = ``,
+  clickedCounter = 0;
 
 const selectClass = (a) => document.querySelector(`.${a}`),
   selectAll = (a) => document.querySelectorAll(`.${a}`),
@@ -48,6 +50,24 @@ const calculateAdjBombs = function () {
   }
 };
 
+const countForWin = function () {
+  clickedCounter--;
+  console.log(clickedCounter);
+  if (clickedCounter === 0) {
+    gameStatus = `won`;
+    endingGame();
+  }
+};
+
+const endingGame = function () {
+  // remove flags and show mines
+  selectClass(`ending-title`).textContent =
+    gameStatus === `won` ? `You won the game!` : `You lost the game!`;
+  selectClass(`ending-title`).classList.remove(`not-shown`);
+  selectClass(`play-again`).classList.remove(`not-shown`);
+  selectClass(`difficulty-options-end`).classList.remove(`not-shown`);
+};
+
 const clearZeros = function (i, y) {
   selectId(`square-${i}-${y}`).classList.add(`visited`);
   for (let a = i - 1; a <= i + 1; a++) {
@@ -55,6 +75,9 @@ const clearZeros = function (i, y) {
     for (let b = y - 1; b <= y + 1; b++) {
       if (b === 0 || b > numberOfColumns) continue;
       let adjunctSquare = document.getElementById(`square-${a}-${b}`);
+      if (adjunctSquare.classList.contains(`unclicked`)) {
+        countForWin();
+      }
       adjunctSquare.classList.remove(`unclicked`);
       nearbyBombs[adjunctSquare.id] === 0
         ? (adjunctSquare.textContent = ``)
@@ -62,8 +85,12 @@ const clearZeros = function (i, y) {
       if (
         nearbyBombs[adjunctSquare.id] === 0 &&
         !adjunctSquare.classList.contains(`visited`)
-      )
+      ) {
+        if (adjunctSquare.classList.contains(`unclicked`)) {
+          countForWin();
+        }
         clearZeros(a, b);
+      }
     }
   }
 };
@@ -73,20 +100,33 @@ const clickingSquares = function () {
     for (let y = 1; y <= numberOfColumns; y++) {
       const clickedSquare = selectId(`square-${i}-${y}`);
       clickedSquare.addEventListener(`auxclick`, () => {
-        clickedSquare.textContent = `🚩`;
-        clickedSquare.classList.add(`flagged`);
+        if (gameStatus === `inProgress`) {
+          if (clickedSquare.classList.contains(`flagged`)) {
+            clickedSquare.classList.remove(`flagged`);
+            clickedSquare.textContent = ``;
+          } else {
+            clickedSquare.textContent = `🚩`;
+            clickedSquare.classList.add(`flagged`);
+          }
+        }
       });
       clickedSquare.addEventListener(`click`, () => {
-        if (!clickedSquare.classList.contains(`flagged`)) {
-          clickedSquare.classList.remove(`unclicked`);
-          if (clickedSquare.classList.contains("bomb")) {
-            clickedSquare.textContent = `💣`;
-          } else if (nearbyBombs[clickedSquare.id] === 0) {
-            clickedSquare.textContent = ``;
-            clickedSquare.classList.add(`visited`);
-            clearZeros(i, y);
-          } else {
-            clickedSquare.textContent = nearbyBombs[clickedSquare.id];
+        if (gameStatus === `inProgress`) {
+          if (!clickedSquare.classList.contains(`flagged`)) {
+            clickedSquare.classList.remove(`unclicked`);
+            if (clickedSquare.classList.contains("bomb")) {
+              clickedSquare.textContent = `💣`;
+              gameStatus = `lost`;
+              endingGame();
+            } else if (nearbyBombs[clickedSquare.id] === 0) {
+              countForWin();
+              clickedSquare.textContent = ``;
+              clickedSquare.classList.add(`visited`);
+              clearZeros(i, y);
+            } else {
+              countForWin();
+              clickedSquare.textContent = nearbyBombs[clickedSquare.id];
+            }
           }
         }
       });
@@ -94,10 +134,19 @@ const clickingSquares = function () {
   }
 };
 const startTheGame = function (nor, noc, nom) {
+  gameStatus = `inProgress`;
+  selectClass(`minefield`).innerHTML = ` `;
   numberOfRows = nor;
   numberOfColumns = noc;
   numberOfMines = nom;
+  clickedCounter = nor * noc - nom;
+  selectClass(`ending-title`).classList.add(`not-shown`);
+  selectClass(`play-again`).classList.add(`not-shown`);
+  selectClass(`difficulty-options-end`).classList.add(`not-shown`);
   selectClass(`minefield`).classList.remove(`hidden`);
+  selectClass(`ending-title`).classList.remove(`hidden`);
+  selectClass(`play-again`).classList.remove(`hidden`);
+  selectClass(`difficulty-options-end`).classList.remove(`hidden`);
   selectClass(`minefield`).style.gridTemplateColumns = `repeat(${noc}, 1fr)`;
   selectClass(`minefield`).style.gridTemplateRows = `repeat(${nor}, 1fr)`;
   selectClass(`introscreen`).classList.add(`hidden`);
